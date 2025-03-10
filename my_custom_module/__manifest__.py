2631365ac94864345735788fe7948f1178d37acf
saenzdf <64671666+saenzdf@users.noreply.github.com>
2025-03-10 17:11:06 -0500

    Create __manifest__.py

---
+++ my_custom_module/__manifest__.py
@@ -0,0 +1,11 @@
+                                                  
+{
+    'name': 'Set Company on product & pos category',
+    'version': '1.0',
+    'depends': ['product', 'point_of_sale'],
+    'data': [
+        'views/product_category_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
